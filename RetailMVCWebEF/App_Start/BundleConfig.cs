c4e8126b29accb5124234f25c4851c179166f20b
zebn <zebn@bk.ru>
2023-05-13 21:37:25 +0200

    fixed jquery

--- RetailMVCWebEF/App_Start/BundleConfig.cs
+++ RetailMVCWebEF/App_Start/BundleConfig.cs
@@ -9,7 +9,7 @@ namespace RetailMVCWebEF
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/jquery-3.4.1.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
